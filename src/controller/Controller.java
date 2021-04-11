7653d9a7ae06ada5f6a6012b5ff208fd21aea0f1
LINA <lina@Laina.local>
2021-04-11 17:14:43 -0500

    cuarto commit

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,24 +1,42 @@
 package controller;
 
+import model.Circle;
 import model.Logic;
+import model.Square;
 import processing.core.PApplet;
 
-
 public class Controller {
-	
-    Logic logic;
 
+	Logic logic;
 	PApplet app;
-	
+	boolean filtro;
 
-	public Controller(PApplet app) {	
-		this.app=app;
-		
+	public Controller(PApplet app) {
+		this.app = app;
+
 		logic = new Logic(app);
+		filtro = true;
 	}
 
 	public void draw() {
 		logic.draw();
+
+	}
+
+	public void newFigure() {
+
+//		if (filtro == true) {
+//			Circle actual = new Circle((int) (10 + (Math.random() * 80)), mouseX, mouseY, 5,
+//					(int) (Math.random() * 35));
+//			logic.getCircle().add(actual);
+//			filtro = false;
+//		} else {
+//			Square actual = new Square((int) (10 + (Math.random() * 80)), mouseX, mouseY, 5,
+//					(int) ((1 + Math.random() * 35)));
+//			logic.getSquare().add(actual);
+//			filtro = true;
+//		}
+//	
 
 	}
 
--- src/model/Shapes.java
+++ src/model/Shapes.java
@@ -17,5 +17,97 @@ public abstract class Shapes {
 
 	public abstract void draw();
 
-	public abstract void move();
+	public void move() {
+		
+		posX += directionA;
+		posY += directionB;
+		
+	}
+	
+	public void limit() {
+		
+		if(posX-tam/2<0) {
+			directionA = 1;
+		}
+		
+		if(posY-tam/2<0) {
+			directionA = 1;
+		}
+		
+		if(posX+tam/2>500) {
+			directionA = -1 ;
+		}
+		
+		if(posY+tam/2>500) {
+			directionA = -1;
+		}
+		
+	}
+
+	public String getWords() {
+		return words;
+	}
+
+	public void setWords(String words) {
+		this.words = words;
+	}
+
+	public PApplet getApp() {
+		return app;
+	}
+
+	public void setApp(PApplet app) {
+		this.app = app;
+	}
+
+	public int getTam() {
+		return tam;
+	}
+
+	public void setTam(int tam) {
+		this.tam = tam;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public int getDirectionA() {
+		return directionA;
+	}
+
+	public void setDirectionA(int directionA) {
+		this.directionA = directionA;
+	}
+
+	public int getDirectionB() {
+		return directionB;
+	}
+
+	public void setDirectionB(int directionB) {
+		this.directionB = directionB;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
+	
 }
